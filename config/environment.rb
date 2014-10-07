c81f973270f57ce4c42860ac62c011be635d5455
Justin Kronz <jkronz@gmail.com>
2014-10-07 09:47:39 -0400

    Last adjustments to images

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-ENV['RAILS_RELATIVE_URL_ROOT'] = "/afm"
-
 # Initialize the Rails application.
 Rails.application.initialize!
